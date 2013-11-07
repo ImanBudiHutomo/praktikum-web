0d414b84b86c7ae683e6028cfc622ac1083a35c6
Iman Budi Hutomo <iman.280793@gmail.com>
2013-11-07 11:38:37 +0700

    Delete koneksi.php

--- 110533406989/Pertemuan Ke-5/Tugas Praktikum/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host = 'mysql7.000webhost.com'; $user = 'a1566486_root'; $pass = 'iman280793';$db = 'a1566486_myweb';
-$cnn = mysql_connect($host, $user, $pass);
-if (!$cnn) { exit('Koneksi Gagal'); }
-$db = mysql_select_db($db);
-if (!$db) { exit('Gagal Memilih Database'); }
-?>
