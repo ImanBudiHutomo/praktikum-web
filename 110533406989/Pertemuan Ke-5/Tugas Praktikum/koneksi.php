77a5eb8eee4096cae056b98fddeae8a6b8ef8cbe
Iman Budi Hutomo <iman.280793@gmail.com>
2013-11-07 11:18:57 +0700

    Laporan 6
    110533406989_Iman Budi Hutomo

--- 110533406989/Pertemuan Ke-5/Tugas Praktikum/koneksi.php
+++ 110533406989/Pertemuan Ke-5/Tugas Praktikum/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$host = 'localhost'; $user = 'root'; $pass = '';$db = 'myweb';
+$host = 'mysql7.000webhost.com'; $user = 'a1566486_root'; $pass = 'iman280793';$db = 'a1566486_myweb';
 $cnn = mysql_connect($host, $user, $pass);
 if (!$cnn) { exit('Koneksi Gagal'); }
 $db = mysql_select_db($db);
